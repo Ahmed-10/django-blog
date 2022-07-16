6ccfd4c5cf357e4679156fff02b7600066d303a3
ahmed-10 <ahmedalaa.10@hotmail.com>
2022-07-16 19:24:21 +0200

    feat: update user profile

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', views.logout, name='logout'),
     path("profile/", views.profile, name='profile'),
+    path("profile/update", views.update_profile, name='update-profile')
 ]
